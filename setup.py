f3330f9d6c609270fa568d5ba93937f6a417302f
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-12-17 10:24:03 +0530

    Update Folder structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='KyberKEM-py',
+    packages=find_packages(include=['KyberKEM-py']),
+    version='0.1.0',
+    description='Python Implementation of KyberKEM, a Post Quantum Cryptography Algorithm',
+    author='Subhajit Biswas & Rohan Kalra',
+    install_requires=[
+        'numpy',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.4.3'],
+    test_suite='tests',
+)
