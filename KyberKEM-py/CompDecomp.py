a795fee7dc3b2d002f1ce05810e935a5cc7b78eb
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-12-18 12:32:31 +0530

    Add encode decode

--- KyberKEM-py/CompDecomp.py
+++ KyberKEM-py/CompDecomp.py
@@ -1,11 +1,12 @@
 """
 The code for compression and decompression step of polynomials
 """
-from utility import round_up
+from utility import round_up, bitstring_to_bytes, bytes_to_bits
 
 def compress(x,d,q):
     """
-    
+    Compress the polynomial by compressing each coefficent
+    NOTE: This is lossy compression
     """
     compress_mod = 2**d
     compress_float = compress_mod/q
@@ -21,6 +22,26 @@ def decompress(x,d,q):
     decompress_float = q/2**d
     return (decompress_float*x)
 
-# def encode():
+# print(compress(6,3,16))
+# print(decompress(3,3,16))
 
-# def decode():
+def encode(coef, l=None):
+    if l is None:
+        l = max(x.bit_length() for x in coef)
+    bit_string = ''.join(format(c,f'0{1}b')[::-1] for c in coef)
+    return bitstring_to_bytes(bit_string)
+
+
+def decode(n, input_bytes, l=None, is_ntt=False):
+    if l is None:
+            l, check = divmod(8*len(input_bytes), n)
+            if check != 0:
+                raise ValueError("input bytes must be a multiple of (polynomial degree) / 8")
+    else:
+        if n*l != len(input_bytes)*8:
+            raise ValueError("input bytes must be a multiple of (polynomial degree) / 8")
+    coefficients = [0 for _ in range(n)]
+    list_of_bits = bytes_to_bits(input_bytes)
+    for i in range(n):
+        coefficients[i] = sum(list_of_bits[i*l + j] << j for j in range(l))
+    return coefficients, is_ntt
