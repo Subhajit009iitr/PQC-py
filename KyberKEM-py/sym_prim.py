0b7ffe414549fc4acbb09edee22f425d5835b93d
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-12-30 23:14:37 +0530

    Finalize sym_prim.py

--- KyberKEM-py/sym_prim.py
+++ KyberKEM-py/sym_prim.py
@@ -5,19 +5,13 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 
-key = b'cookie'
-data = b'cake'
-
 def PRF(key, data):
     prf_output = hmac.new(key, data, hashlib.sha256).digest()
-    print(prf_output)
-    print(sys.getsizeof(prf_output))
+    return prf_output
 
 def XOF(data):
     xof_output = hashlib.sha3_256(data).digest()
-    print(xof_output)
-    print(sys.getsizeof(xof_output))
-
+    return xof_output
 def KDF(key):
     kdf = HKDF(
     algorithm=hashes.SHA256(),
@@ -27,6 +21,4 @@ def KDF(key):
     backend=default_backend()
     )
     derived_key = kdf.derive(key)
-    print(derived_key)
-
-KDF(key)
+    return derived_key
